4e4ea18ed85a4bb0cc2de2a7ea7f33f43ce9c387
Sujan Sharma <138952788+NotSujanSharma@users.noreply.github.com>
2024-11-05 15:01:56 -0500

    Update AuthContext.js

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
       formData.append('username', username);
       formData.append('password', password);
 
-      const response = await fetch('http://ak.coitus.ca:9292/token', {
+      const response = await fetch('https://api.bigcityops.ca//token', {
         method: 'POST',
         body: formData,
       });
