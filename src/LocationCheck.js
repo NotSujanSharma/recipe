5c75d76fe44ca3636d560ad2d2618a894eb41f1a
NotSujanSharma <ssharma627@myseneca.ca>
2024-11-05 00:49:43 -0500

    removed unnecessary import

--- src/LocationCheck.js
+++ src/LocationCheck.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
 
 const LocationCheck = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
